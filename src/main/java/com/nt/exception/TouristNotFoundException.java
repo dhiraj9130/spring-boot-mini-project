03d68031d2507f2ff52c00049c08050eca711f29
dhiraj9130 <128839152+dhiraj9130@users.noreply.github.com>
2023-04-19 22:54:58 +0530

    Update TouristNotFoundException.java

--- src/main/java/com/nt/exception/TouristNotFoundException.java
+++ src/main/java/com/nt/exception/TouristNotFoundException.java
@@ -2,7 +2,7 @@ package com.nt.exception;
 
 public class TouristNotFoundException extends Exception {
 	
-	private static final long serialVersionUID = 1L;
+	//private static final long serialVersionUID = 1L;
 
 	public TouristNotFoundException(String msg) {
 		super(msg);
